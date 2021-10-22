06f70c0105dc450ac294d8b892acb1b42a35a7a9
shakil1819 <58840439+shakil1819@users.noreply.github.com>
2021-10-22 17:57:10 +0600

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,14 @@
+public class StudentDetails
+{
+    public void StudentAge()
+    {   //local variable age
+        int age=0;
+        age=age+5;
+        System.out.println("Student age is : "+age);
+    }
+    public static void main(String args[])
+    {
+        StudentDetails obj= new StudentDetails();
+        obj.StudentAge();
+    }
+}
